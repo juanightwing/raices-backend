1067814d3812a790f897bdc96b0ae0de7ed86994
Juan Vasquez <trosco22@gmail.com>
2026-05-11 17:55:35 -0500

    Agrega SSL para conexion Railway en produccion

--- src/database.js
+++ src/database.js
@@ -9,7 +9,12 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
